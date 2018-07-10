517dff1a4d95a8164fa4830bc33d06388fd53262
Adrian Gjerstad <adriangjerstad@lances-mac-mini.lan>
2018-07-10 12:06:52 -0400

    Put ApplePickerUpgrade class in seperate file.

--- ApplesnGold.cpp
+++ ApplesnGold.cpp
@@ -12,6 +12,8 @@
 
 #include <sys/stat.h>
 
+#include "ApplePickerUpgrade.h"
+
 std::string name;
 std::string filename;
 int apples = 0;
@@ -22,53 +24,6 @@ int roundNum = 0;
 int platinum = 0;
 int platinumPrestige = 0;
 float lifetimeGold = 0;
-
-template <typename... Args>
-std::string string_format(const std::string &format, Args... args) {
-  size_t size =
-      snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
-  std::unique_ptr<char[]> buf(new char[size]);
-  snprintf(buf.get(), size, format.c_str(), args...);
-  return std::string(buf.get(),
-                     buf.get() + size - 1); // We don't want the '\0' inside
-}
-
-class ApplePickerUpgrade {
-public:
-  ApplePickerUpgrade(std::string name, int multiplier, int max)
-      : name_(std::move(name)), multiplier_(multiplier), max_(max), level_(0) {}
-
-  bool upgrade() {
-    if (level_ < max_) {
-      ++level_;
-      std::cout << std::endl << "You bought a(n) " << name_ << "!" << std::endl;
-      return true;
-    }
-    std::cout << std::endl << "Oops! It is at it's max level!" << std::endl;
-    return false;
-  }
-
-  float cost() { return multiplier_ * (level_ + 1) * .5; }
-
-  int pick() { return multiplier_ * level_; }
-
-  int level() { return level_; }
-
-  void load(int level) { level_ = std::min(level, max_); }
-
-  std::string name() { return name_; }
-
-  std::string StoreLabel() {
-    return string_format("%s - %0.02f Gold - %d/%d", name_.c_str(), cost(),
-                         level_, max_);
-  }
-
-private:
-  const std::string name_;
-  const int multiplier_;
-  const int max_;
-  int level_;
-};
 
 ApplePickerUpgrade applePickers("Apple Picker", 1, 10);
 ApplePickerUpgrade wizards("Wizard", 2, 10);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ApplesNGold
+# Apples'n'Gold
+
+Apples'n'Gold is a simple console computer game written by Adrian Gjerstad and Lance Gjerstad. This game has many fun aspects to it, and is being built on day after day. For example, a shop, prestiging, trades, and many more things! Here on GitHub, you are able to view the code that makes it possible! This game is originally by [CodeDojo](https://github.com/CodeDojoOfficial), and it grows alongside it. Adrian is a part of CodeDojo, so no copyright infringement is made.
